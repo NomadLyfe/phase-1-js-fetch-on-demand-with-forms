58ffeee6adc78da95e50c7ded1c80f5a0016c019
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-04-16 22:11:33 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const input = inputForm.querySelector('#searchByID').value;
+        fetch(`http://localhost:3000/movies/${input}`)
+            .then(resp => resp.json())
+            .then(data => {
+                const title = document.querySelector('section#movieDetails h4');
+                const summary = document.querySelector('section#movieDetails p');
+                title.textContent = data.title;
+                summary.textContent = data.summary;
+            });
+        inputForm.reset();
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
